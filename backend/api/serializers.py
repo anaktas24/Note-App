98caef6e9efb0468e036f0ea2512012753f963c0
Ioannis Antoniou <johnantony24@hotmail.com>
2024-05-23 19:23:02 +0200

    making the paths

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,3 +3,18 @@ from rest_framework import serializers
 
 #ORM - Object Relational Mapping
 #Api -> JSON -> Serializer -> Response
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'required': True
+            }
+        }
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+# creates a user object with the validated data
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all() #list of all different objects
+    serializer_class = User #what data to accept
+    permission_classes = (AllowAny,) #who can call

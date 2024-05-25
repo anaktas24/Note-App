5349366579a48c8789cb8f64558fb1f39039ebc6
Ioannis Antoniou <johnantony24@hotmail.com>
2024-05-25 23:30:23 +0200

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <>
-          <h1>React App</h1>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </>
-        <h1>React App</h1>
+
       </header>
     </div>
 

f34e3f9dcb7be33be609689767585af287e56d9a
Ioannis Antoniou <johnantony24@hotmail.com>
2024-05-25 23:29:27 +0200

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <>
+          <h1>React App</h1>
+          <h2>React App</h2>
+          <h3>React App</h3>
+          <h4>React App</h4>
+          <h5>React App</h5>
+          <h6>React App</h6>
+        </>
         <h1>React App</h1>
       </header>
     </div>

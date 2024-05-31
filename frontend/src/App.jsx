dd16f8d043e36163059debd4f3c63157666fb81b
Ioannis Antoniou <johnantony24@hotmail.com>
2024-05-31 22:17:07 +0200

    Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 
+//logout
+function logout(){
+  localStorage.clear();
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout(){
+  localStorage.clear();
+  return <Register />
+}
 function App() {
   return (
-    <>
-    </>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 
 }

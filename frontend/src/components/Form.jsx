31ebeeeb027d77e900e707296655a8cbff7e5701
Ioannis Antoniou <johnantony24@hotmail.com>
2024-06-01 21:45:53 +0200

    Form page

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import api from '../api';
 import {useNavigate} from 'react-router-dom';
-import { ACCESS_TOKEN } from '../constants';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants';
 
 function Form({route, method}) {
   const [username, setUsername] = useState('');
@@ -12,7 +12,25 @@ function Form({route, method}) {
   const name =method === "login" ? "Login" : "Register";
 
   const handleSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
+
+    try {
+      const response = await api.post(route, {username, password})
+      if(method === "login") {
+        localStorage.setItem(ACCESS_TOKEN, response.data.access);
+        localStorage.setItem(REFRESH_TOKEN, response.data.refresh);
+        navigate('/');
+      }else {
+        navigate('/login');
+      }
+    }catch (error) {
+      alert(error);
+    }
+    finally {
+      setLoading(false);
+    }
+
   }
 
   return <form onSubmit={handleSubmit} className="form-container">
@@ -24,5 +42,13 @@ function Form({route, method}) {
       value={username}
       onChange={(e) => setUsername(e.target.value)}
     />
+    <input
+      className="form-input"
+      type="text"
+      placeholder="Password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+    />
+    <button className='form-button' type="submit">{name}</button>
   </form>
 }

ccf4ed64042d972c4a504a0ad270e201623e9d43
Ioannis Antoniou <johnantony24@hotmail.com>
2024-06-04 23:27:01 +0200

    Form css

--- frontend/src/components/Loadingindicator.jsx
+++ frontend/src/components/Loadingindicator.jsx
@@ -1,1 +1,14 @@
 import "../styles/Loadingindicator.css";
+
+
+const LoadingIndicator = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading">
+
+      </div>
+    </div>
+  );
+}
+
+export default LoadingIndicator;
